68defa5b75a58bf5c98a561bcab53b27c645c979
caplinje-NOAA <james.caplinger@NOAA.gov>
2024-11-24 14:42:30 -0500

    replace -infs with nan in decidecade_sel

--- pypam/signal.py
+++ pypam/signal.py
@@ -420,7 +420,7 @@ class Signal:
         for i,(center,high,low) in enumerate(zip(centers,highs,lows)):
             indices = np.where((f_psd > low) & (f_psd < high))
             Ef_band = np.trapz(Ef[indices],x=f_psd[indices])
-            Ef_band = np.nan_to_num(Ef_band)
+            Ef_band[Ef_band==-np.inf]=np.nan # replace -infs with nans for log10
             sel[i] = 10 * np.log10( Ef_band )
         f = centers
         return f,sel
